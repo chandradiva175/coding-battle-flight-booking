61988ac6e9ea1555904509c4ea5600b8baf7c005
Fredilla Prima Chandradiva <fredilla.p@leecare.com.au>
2025-05-22 03:54:32 +0700

    initial commit

---
+++ src/main/java/com/mitrais/flight/booking/CodingBattleFlightBookingApplication.java
@@ -0,0 +1,13 @@
+package com.mitrais.flight.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingBattleFlightBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingBattleFlightBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mitrais/flight/booking/CodingBattleFlightBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitrais.flight.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingBattleFlightBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
